3c1b45d680a495b07769df76d2c680fcccf0ae53
PhapNguyen <vietphappro1@gmail.com>
2024-02-20 15:01:49 +0700

    update relationship entities

--- src/main/java/SocialMedia/Entities/Account.java
+++ src/main/java/SocialMedia/Entities/Account.java
@@ -3,13 +3,18 @@ package SocialMedia.Entities;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -58,4 +63,46 @@ public class Account implements Serializable{
 	
 	@Column
 	private boolean isBanned;
+	
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "Account_AccountType",
+			joinColumns = {@JoinColumn(name = "username")},
+			inverseJoinColumns = {@JoinColumn(name = "typeId")})
+	private Set<AccountType> accountTypes;
+	
+	@OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+	private Set<Notification> notifications;
+	
+	@OneToMany(mappedBy = "posterAccount", fetch = FetchType.LAZY)
+	private List<Post> posts;
+	
+	@OneToMany(mappedBy = "sharerAccount", fetch = FetchType.LAZY)
+	private List<Post> shares;
+	
+	@OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+	private Set<Comment> comments;
+	
+	@OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+	private Set<Friend> friends;
+	
+	@ManyToMany
+	@JoinTable(name = "Account_SocialGroup",
+		joinColumns = {@JoinColumn(name = "username") },
+		inverseJoinColumns = {@JoinColumn(name = "groupId")})
+	private Set<SocialGroup> socialGroups;
+	
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "Account_Conversation",
+		joinColumns = {@JoinColumn(name = "username") },
+		inverseJoinColumns = {@JoinColumn(name = "conversationId")})
+	private Set<Conversation> conversations;
+	
+	@ManyToMany
+	@JoinTable(name = "Account_Block_Account",
+		joinColumns = {@JoinColumn(name = "username") },
+		inverseJoinColumns = {@JoinColumn(name = "blockUsername")})
+	private Set<Account> BlockAccounts;
+	
+	
 }
